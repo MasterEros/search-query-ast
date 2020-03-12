191e6e543808208a18e3acafd43c7e4a64369f31
MasterEros <gtxyd@mail.ru>
2020-03-12 22:44:21 +0300

    buildNormalizationMap +
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/normalization.js
+++ src/normalization.js
@@ -1,59 +1,62 @@
 // const value = "some   other  value";
-// const words = normalizeString(value).split(" ");
+// // const words = normalizeString(value).split(" ");
 
-const value = "   some other  value";
+// // const value = "   some other  value";
+// const length_value = value.length - 1;
+// const words = value.split(" ");
+// const map = [];
+// var index = null;
 
-const length_value = value.length - 1;
-const words = value.split(" ");
+// const length_words = words.length;
 
-const map = [];
-var counter = 0,
-	index = null,
-	length = null;
+// for (let i = 0; i < length_value; i++) {
+// 	if (value[i] === " ") {
+// 		index = i;
+// 		for (let j = i; j < length_value; j++) {
+// 			if (value[j] !== " ") {
+// 				var length = j - i;
+// 				if (length > 1) {
+// 					map.push({
+// 						index: i,
+// 						length: length
+// 					});
+// 				}
+// 				i = j;
+// 				break;
+// 			}
+// 		}
+// 	}
+// }
 
-for (var i = 0; i < value.length; i++) {
-	counter = value[i] === " " ? (counter += 1) : 0;
-
-	// if (counter !== 0 && index === null) {
-	// 	index = i;
-	// 	i;
-	// } else if (counter === 0 && index !== null) {
-	// 	index = null;
-	// 	i;
-	// }
-
-	counter;
-}
-
-length_value;
-words;
-map;
+// length_value;
+// words;
+// map;
 
 export function normalizeString(value) {
 	return value.trim().replace(/\s{2,}/g, " ");
 }
 export function buildNormalizationMap(value) {
-	// return [
-	// 	{
-	// 		index: 0,
-	// 		length: 3
-	// 	},
-	// 	{
-	// 		index: 13,
-	// 		length: 2
-	// 	}
-	// ];
-	//----------------------------------------------------------------
-	// return [
-	// 	{
-	// 		index: 4,
-	// 		length: 3
-	// 	},
-	// 	{
-	// 		index: 12,
-	// 		length: 2
-	// 	}
-	// ];
+	const map = [];
+	var index = null;
+	for (let i = 0; i < value.length; i++) {
+		if (value[i] === " ") {
+			index = i;
+			for (let j = i; j < value.length; j++) {
+				if (value[j] !== " ") {
+					var length = j - i;
+					if (length > 1) {
+						map.push({
+							index: i,
+							length: length
+						});
+					}
+					i = j;
+					break;
+				}
+			}
+		}
+	}
+	return map;
 }
 export function buildPositionRestorer(map) {}
 export function normalizeTokens(tokens) {}
