47a554ea3fc794f457694a8a09f7681bd6108953
MasterEros <gtxyd@mail.ru>
2020-03-14 17:45:17 +0300

    backup
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/normalization.js
+++ src/normalization.js
@@ -1,15 +1,8 @@
 // @ts-nocheck
-// const value = "some   other  value";
-// const value = " some other   value ";
-// const value = "   some  some     value  some ";
-
-// const value = " some other   value ";
-// const map = buildNormalizationMap(value);
 
 const map = buildNormalizationMap("some   other  value");
 const restorer = buildPositionRestorer(map);
-const result = restorer(11);
-result;
+const result = restorer(11); // 11 - 14, 7 - 9
 
 export function normalizeString(value) {
 	return value.trim().replace(/\s{2,}/g, " ");
@@ -35,64 +28,10 @@ export function buildNormalizationMap(value) {
 }
 export function buildPositionRestorer(map) {
 	return function(value) {
-		return map;
+		
+
+		value;
+		map;
 	};
 }
 export function normalizeTokens(tokens) {}
-
-// const map = buildNormalizationMap(value);
-// const map_restorer = buildPositionRestorer(map);
-// const result = map_restorer(11);
-// result;
-
-// for (let i = 0; i < value.length; i++) {
-// 	if (value[i] === " ") {
-// 		for (let j = i; j < value.length; j++) {
-// 			if (value[j] !== " ") {
-// 				var length = j - i;
-// 				length;
-// 				if (length === 1 && i === 0) {
-// 					i;
-// 				} else if (length === 1 && i === value.length - 1) {
-// 					i;
-// 				} else if (length > 1) {
-// 					map.push({
-// 						index: i,
-// 						length: length
-// 					});
-// 					i = j;
-// 				}
-// 				break;
-// 			}
-// 		}
-// 	}
-// }
-
-// const words = value.split(" ");
-// words;
-// // Сравнение строк и использование normalizeString + indexOf + IndexFirstOf
-// var count = 0;
-// for (let index in words) {
-// 	const numindex = Number(index);
-// 	const word_length = words[numindex].length;
-// 	if (word_length === 0 && words[numindex] !== words[numindex + 1]) {
-// 		// count;
-// 		// count = index - count + 1;
-// 		// count;
-// 	} else if (word_length === 0 && words[numindex] !== words[numindex - 1]) {
-// 		index;
-// 		// count = index;
-// 	}
-// }
-
-// const value = "some   other  value";
-// const value = "   some other  value";
-// var restorer = 0;
-// for (let i = 0; i < map.length; i++) {
-// 	var map_count = map[i].index + map[i].length;
-// 	map_count;
-// }
-// restorer;
-// expect(restorer(0)).toBe(1);
-// expect(restorer(13)).toBe(16);
-// Позиция + восстановление пробелов
