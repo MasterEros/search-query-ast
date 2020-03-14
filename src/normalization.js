601e85c4ae3938c0425a1d546b7df4a585c11010
MasterEros <gtxyd@mail.ru>
2020-03-14 17:36:06 +0300

    buildNormalizationMap +
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/normalization.js
+++ src/normalization.js
@@ -1,48 +1,36 @@
 // @ts-nocheck
 // const value = "some   other  value";
-// const value = "   some other  value";
-
-// var restorer = 0;
-// for (let i = 0; i < map.length; i++) {
-// 	var map_count = map[i].index + map[i].length;
-// 	map_count;
-// }
-// restorer;
-
-// expect(restorer(0)).toBe(1);
-// expect(restorer(13)).toBe(16);
-
-// Позиция + восстановление пробелов
-
-// const value = "some   other  value";
 // const value = " some other   value ";
-const value = "   some  some     value  some ";
-const map = buildNormalizationMap(value);
+// const value = "   some  some     value  some ";
+
+// const value = " some other   value ";
+// const map = buildNormalizationMap(value);
+
+const map = buildNormalizationMap("some   other  value");
+const restorer = buildPositionRestorer(map);
+const result = restorer(11);
+result;
 
 export function normalizeString(value) {
 	return value.trim().replace(/\s{2,}/g, " ");
 }
 export function buildNormalizationMap(value) {
 	const map = [];
-	const words = value.split(" ");
-	words;
-
 	var count = 0;
-	for (let index in words) {
-		const numindex = Number(index);
-		const word_length = words[numindex].length;
-
-		if (word_length === 0 && words[numindex] !== words[numindex + 1]) {
-			// count;
-			// count = index - count + 1;
-			// count;
-		} else if (word_length === 0 && words[numindex] !== words[numindex - 1]) {
-			index;
-			// count = index;
+	for (var i = 0; i <= value.length; i++) {
+		if (value[i] === " ") {
+			count++;
+		} else {
+			if (
+				(count === 1 && i - 1 === 0) ||
+				(count === 1 && i - 1 === value.length - 1) ||
+				count > 1
+			) {
+				map.push({ index: i - count, length: count });
+			}
+			count = 0;
 		}
 	}
-
-	map;
 	return map;
 }
 export function buildPositionRestorer(map) {
@@ -79,3 +67,32 @@ export function normalizeTokens(tokens) {}
 // 		}
 // 	}
 // }
+
+// const words = value.split(" ");
+// words;
+// // Сравнение строк и использование normalizeString + indexOf + IndexFirstOf
+// var count = 0;
+// for (let index in words) {
+// 	const numindex = Number(index);
+// 	const word_length = words[numindex].length;
+// 	if (word_length === 0 && words[numindex] !== words[numindex + 1]) {
+// 		// count;
+// 		// count = index - count + 1;
+// 		// count;
+// 	} else if (word_length === 0 && words[numindex] !== words[numindex - 1]) {
+// 		index;
+// 		// count = index;
+// 	}
+// }
+
+// const value = "some   other  value";
+// const value = "   some other  value";
+// var restorer = 0;
+// for (let i = 0; i < map.length; i++) {
+// 	var map_count = map[i].index + map[i].length;
+// 	map_count;
+// }
+// restorer;
+// expect(restorer(0)).toBe(1);
+// expect(restorer(13)).toBe(16);
+// Позиция + восстановление пробелов
