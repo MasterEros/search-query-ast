a0362b0e153e4d2e230236ffd0ea5e5e5af637ee
MasterEros <gtxyd@mail.ru>
2020-03-19 23:50:29 +0300

    backup
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/token/token.js
+++ src/token/token.js
@@ -1,42 +1,92 @@
 // const data = tokenize("foo OR baz AND bar");
-const data = tokenize("some value OR other value");
+const data = tokenize("some  value OR other value");
 
 // "foo OR baz AND bar" [{"position": 0, "type": "item", "value": "foo"}, {"position": 4, "type": "binary_operator", "value": "OR"}, { "position": 7, "type": "item", "value": "baz" }, { "position": 11, "type": "binary_operator", "value": "AND" }, { "position": 15, "type": "item", "value": "bar" }]
 
 // "some value OR other value" [{"position": 0, "type": "item", "value": "some value"}, {"position": 11, "type": "binary_operator", "value": "OR"}, {"position": 14, "type": "item", "value": "other value"}]
 
+data;
+
 export function tokenize(str) {
 	const map = [];
-	const words = str
-		.trim()
-		.replace(/\s{2,}/g, " ")
-		.split(" ");
+	const words = str.split(/\ |\(|\)/);
 
+	words;
 	words.forEach((item, index) => {
-		if (
-			item &&
-			item !== "OR" &&
-			item !== "AND" &&
-			words[index + 1] &&
-			words[index + 1] !== "OR" &&
-			words[index + 1] !== "AND"
-		) {
-			map.push({
-				position: str.indexOf(item),
-				type: "item",
-				value: item + " " + words[index + 1]
-			});
-		} else if (item === "OR" || item === "AND") {
+		if (item === "OR" || item === "AND") {
 			map.push({
 				position: str.indexOf(item),
 				type: "binary_operator",
 				value: item
 			});
+		} else if (item === "(") {
+			map.push({
+				position: str.indexOf(item),
+				type: "left_bracket",
+				value: "("
+			});
+		} else if (item === ")") {
+			map.push({
+				position: str.indexOf(item),
+				type: "right_bracket",
+				value: ")"
+			});
+		} else if (item === "") {
+			map.push({
+				position: str.indexOf(item),
+				type: "space",
+				value: " "
+			});
 		} else {
+			index;
+			item;
 		}
 	});
 
-	map;
-
-	// return str;
+	return map;
 }
+// 	if (
+// 		item &&
+// 		item !== " " &&
+// 		item !== "(" &&
+// 		item !== ")" &&
+// 		item !== "OR" &&
+// 		item !== "AND" &&
+// 		words[index + 1] &&
+// 		words[index + 1] !== " " &&
+// 		words[index + 1] !== "(" &&
+// 		words[index + 1] !== ")" &&
+// 		words[index + 1] !== "AND" &&
+// 		words[index + 1] !== "OR"
+// 	) {
+// 		map.push({
+// 			position: str.indexOf(item),
+// 			type: "item",
+// 			value: item + " " + words[index + 1]
+// 		});
+// 	} else if (item === "OR" || item === "AND") {
+// 		map.push({
+// 			position: str.indexOf(item),
+// 			type: "binary_operator",
+// 			value: item
+// 		});
+// 	} else if (item === "(") {
+// 		map.push({
+// 			position: str.indexOf(item),
+// 			type: "left_bracket",
+// 			value: item
+// 		});
+// 	} else if (item === ")") {
+// 		map.push({
+// 			position: str.indexOf(item),
+// 			type: "right_bracket",
+// 			value: item
+// 		});
+// 	} else if (item === " ") {
+// 		map.push({
+// 			position: str.indexOf(item),
+// 			type: "space",
+// 			value: item
+// 		});
+// 	}
+// });
