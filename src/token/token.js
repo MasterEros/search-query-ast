824bd571c00a4090f3ca0465c80753a42d26edff
MasterEros <gtxyd@mail.ru>
2020-03-27 23:54:15 +0300

    backup
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/token/token.js
+++ src/token/token.js
@@ -1,17 +1,13 @@
-// "foo OR baz AND bar" [{"position": 0, "type": "item", "value": "foo"}, {"position": 4, "type": "binary_operator", "value": "OR"}, { "position": 7, "type": "item", "value": "baz" }, { "position": 11, "type": "binary_operator", "value": "AND" }, { "position": 15, "type": "item", "value": "bar" }]
-
-// "some value OR other value" [{"position": 0, "type": "item", "value": "some value"}, {"position": 11, "type": "binary_operator", "value": "OR"}, {"position": 14, "type": "item", "value": "other value"}]
-
-// const data = tokenize("some value OR other value");
-const data = tokenize("foo OR baz AND bar AND bey");
-console.log(data);
-
 export function tokenize(str) {
 	return str
 		.split(/ OR | AND /)
-		.map(value => {
+		.map((value, i, arr) => {
+			var position = str.indexOf(value);
+			if (arr[i] === arr[i - 1] && i > 0) {
+				position = str.indexOf(value, str.indexOf(arr[i - 1]) + 1);
+			}
 			return {
-				position: str.indexOf(value),
+				position,
 				type: "item",
 				value: value
 			};
