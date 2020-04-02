18a9e38d7ad904e7ee9df623a07d178eb0b1108d
MasterEros <gtxyd@mail.ru>
2020-04-02 16:12:55 +0300

    backup
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/normalization.js
+++ src/normalization.js
@@ -1,28 +1,37 @@
 // @ts-nocheck
 
+// const result = buildNormalizationMap("   some other  value");
+const result = buildNormalizationMap("some   other  value");
+result;
+
 export function normalizeString(value) {
 	return value.trim().replace(/\s{2,}/g, " ");
 }
 export function buildNormalizationMap(value) {
-	const map = [];
-	// let count = 0;
-	// for (var i = 0; i <= value.length; i++) {
-	// 	if (value[i] === " ") {
-	// 		count++;
-	// 	} else {
-	// 		if (
-	// 			(count === 1 && i - 1 === 0) ||
-	// 			(count === 1 && i - 1 === value.length - 1) ||
-	// 			count > 1
-	// 		) {
-	// 			map.push({
-	// 				index: i - count,
-	// 				length: count
-	// 			});
-	// 		}
-	// 		count = 0;
-	// 	}
-	// }
+	const words = value.split(/(^ +| {2,})/g);
+	words;
+
+	const map = words
+		.map((word, i, arr) => {
+			word;
+
+			if (word[0] === " " && word !== "") {
+				i;
+				word;
+
+				const index = words
+					.filter((word, j) => i < j)
+					.reduce((counter, word) => {
+						return counter;
+					});
+
+				console.log(index);
+
+				return { length: word.length };
+			}
+		})
+		.filter(word => word !== undefined);
+
 	return map;
 }
 export function buildPositionRestorer(map) {
@@ -30,12 +39,12 @@ export function buildPositionRestorer(map) {
 		return (
 			value +
 			map.reduce((counter, item) => {
-				if (value >= item.index && item.index === 0) {
-					counter += item.length;
-				} else if (value >= item.index - counter) {
-					counter += item.length - 1;
-				}
-				return counter;
+				return (counter =
+					value >= item.index && item.index === 0
+						? counter + item.length
+						: value >= item.index - counter
+						? counter + item.length - 1
+						: counter);
 			}, 0)
 		);
 	};
@@ -62,3 +71,10 @@ export function normalizeTokens(tokens) {
 // 		count = 0;
 // 	}
 // }
+
+// if (value >= item.index && item.index === 0) {
+// 	counter += item.length;
+// } else if (value >= item.index - counter) {
+// 	counter += item.length - 1;
+// }
+// return counter;
--- src/token/token.js
+++ src/token/token.js
@@ -1,32 +1,32 @@
 export function tokenize(str) {
-	return str
-		.split(/ OR | AND /)
-		.map((value, i, arr) => {
-			var position = str.indexOf(value);
-			if (arr[i] === arr[i - 1] && i > 0) {
-				position = str.indexOf(value, str.indexOf(arr[i - 1]) + 1);
-			}
-			return {
-				position,
-				type: "item",
-				value: value
-			};
-		})
-		.concat(
-			str
-				.split(" ")
-				.filter(binary => binary === "OR" || binary === "AND")
-				.map((binary, i, arr) => {
-					var position = str.indexOf(binary);
-					if (arr[i] === arr[i - 1] && i > 0) {
-						position = str.indexOf(binary, str.indexOf(arr[i - 1]) + 1);
-					}
-					return {
-						position,
-						type: "binary_operator",
-						value: binary
-					};
-				})
-		)
-		.sort((a, b) => (a.position > b.position ? 1 : -1));
+	// return str
+	// 	.split(/ OR | AND /)
+	// 	.map((value, i, arr) => {
+	// 		var position = str.indexOf(value);
+	// 		if (arr[i] === arr[i - 1] && i > 0) {
+	// 			position = str.indexOf(value, str.indexOf(arr[i - 1]) + 1);
+	// 		}
+	// 		return {
+	// 			position,
+	// 			type: "item",
+	// 			value: value
+	// 		};
+	// 	})
+	// 	.concat(
+	// 		str
+	// 			.split(" ")
+	// 			.filter(binary => binary === "OR" || binary === "AND")
+	// 			.map((binary, i, arr) => {
+	// 				var position = str.indexOf(binary);
+	// 				if (arr[i] === arr[i - 1] && i > 0) {
+	// 					position = str.indexOf(binary, str.indexOf(arr[i - 1]) + 1);
+	// 				}
+	// 				return {
+	// 					position,
+	// 					type: "binary_operator",
+	// 					value: binary
+	// 				};
+	// 			})
+	// 	)
+	// 	.sort((a, b) => (a.position > b.position ? 1 : -1));
 }
