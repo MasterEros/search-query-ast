78a2e1dd57b5928b031ee5c2dbbc429cf92a979b
MasterEros <gtxyd@mail.ru>
2020-04-02 16:33:57 +0300

    backup
    Signed-off-by: MasterEros <gtxyd@mail.ru>

--- src/normalization.js
+++ src/normalization.js
@@ -1,17 +1,13 @@
 // @ts-nocheck
 
-// const result = buildNormalizationMap("   some other  value");
-const result = buildNormalizationMap("some   other  value");
-result;
-
 export function normalizeString(value) {
 	return value.trim().replace(/\s{2,}/g, " ");
 }
 export function buildNormalizationMap(value) {
 	return value
 		.split(/(^ +| {2,})/g)
-		.map((word, i, arr) => {
-			return word[0] === " " && word !== ""
+		.map((word, i, arr) =>
+			word[0] === " " && word !== ""
 				? {
 						index: arr
 							.filter((word, j) => j < i)
@@ -20,51 +16,24 @@ export function buildNormalizationMap(value) {
 							}, 0),
 						length: word.length
 				  }
-				: 0;
-		})
-		.filter(result => result !== 0);
+				: null
+		)
+		.filter(result => result !== null);
 }
 export function buildPositionRestorer(map) {
-	return function(value) {
-		return (
-			value +
-			map.reduce((counter, item) => {
-				return (counter =
+	return value =>
+		value +
+		map.reduce(
+			(counter, item) =>
+				(counter =
 					value >= item.index && item.index === 0
 						? counter + item.length
 						: value >= item.index - counter
 						? counter + item.length - 1
-						: counter);
-			}, 0)
+						: counter),
+			0
 		);
-	};
 }
 export function normalizeTokens(tokens) {
 	// return tokens;
 }
-
-// let count = 0;
-// for (var i = 0; i <= value.length; i++) {
-// 	if (value[i] === " ") {
-// 		count++;
-// 	} else {
-// 		if (
-// 			(count === 1 && i - 1 === 0) ||
-// 			(count === 1 && i - 1 === value.length - 1) ||
-// 			count > 1
-// 		) {
-// 			map.push({
-// 				index: i - count,
-// 				length: count
-// 			});
-// 		}
-// 		count = 0;
-// 	}
-// }
-
-// if (value >= item.index && item.index === 0) {
-// 	counter += item.length;
-// } else if (value >= item.index - counter) {
-// 	counter += item.length - 1;
-// }
-// return counter;
